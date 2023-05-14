124dbb7f13a8b76cc585d2a6335d6b5641d8d164
Dmitry Razumny <nepalevnodigital@gmail.com>
2023-05-14 11:24:11 +0300

    pages

--- assets/HelloWorld-6298e07e.js
+++
@@ -1,1 +0,0 @@
-import{d as o,r as _,c as t,a as r,o as s,b as n,_ as l}from"./index-f156ede1.js";const c=o({__name:"HelloWorld",props:{msg:null},setup(a){return _(0),(p,d)=>{const e=n("router-view");return s(),t("div",null,[r(e)])}}});const m=l(c,[["__scopeId","data-v-3593cf4d"]]);export{m as default};
